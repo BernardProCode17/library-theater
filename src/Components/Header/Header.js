8174d7d1d73bc29a0416641e986da051a7f1fbba
BernardProCode17 <bclarke42@my.bcit.ca>
2023-12-30 12:08:51 -0800

    header styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,16 +1,22 @@
-import logo from "../../logo images/library_theater_logos_transparent.png";
+import './Header.css'
+import logo from "../../logo images/logo.png";
 import { Link } from "react-router-dom";
 function Header() {
 
   return (
-    <header>
-      <nav>
+    <header className="mainHeader">
+      <nav className='Nav'>
+
+        <Link to='/'>
         <img src={logo} alt="Logo of library Theater company" />
+        </Link>
+
         <ul>
         <li><Link to='/' >Home</Link></li>
         <li><Link to='/about' >About</Link></li>
         <li><Link to='/favourites'>Favourites</Link></li>
         </ul>
+        
       </nav>
     </header>
   );
