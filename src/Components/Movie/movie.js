24d0a95e3a949b4b7c63e440b0029cf6cf3ef24d
BernardProCode17 <bclarke42@my.bcit.ca>
2024-01-16 21:57:35 -0800

    Update TypeScript version and add Favourites component, working on add movies to local storage - addind movies in an array

--- src/Components/Movie/movie.js
+++ src/Components/Movie/movie.js
@@ -1,10 +1,11 @@
 import "./movie.css";
 import api from "../Helper/api";
 import { Link } from "react-router-dom";
+import Favourites from "../Helper/favourites";
 
 function Movie({ movie }) {
-  
-  const { title, overview, poster_path, release_date, popularity } = movie || {};
+
+  const { title, overview, poster_path, release_date, popularity, id } = movie || {};
 
   return (
     <article className="MovieArticle">
@@ -14,13 +15,13 @@ function Movie({ movie }) {
         <h3>{title}</h3>
 
         <p>Description: {overview}</p>
-
+        
         <div className="numberInfo">
           <p>Release Date:<br></br>{release_date}</p>
           <p>Rating: <br></br> {((popularity * 100) / 100).toFixed(1)}%</p>
           <p>genre</p>
         </div>
-
+        <Favourites id={movie.id} />
         <Link to={`/moviedetails/${movie.id}`}>
           More Info
         </Link>
